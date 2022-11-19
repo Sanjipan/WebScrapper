65834c06eb36f68272a572ffbd91744db1ebc75c
Sanjipan <sanjipan2003@gmail.com>
2022-11-19 21:47:00 +0530

    initial commit

--- WebScrapperLib.py
+++ WebScrapperLib.py
@@ -1,49 +1,52 @@
-from bs4 import BeautifulSoup
-import requests
-import requests.exceptions
-import urllib.parse
-from collections import deque
 import re
+import urllib.request
+import time
+
+email_file = open("email.txt", 'a')
 
 
-class WebScrapper:
+# Extracting Emails
+def extract_emails_from_url_text(url_text, email_regex):
+    extracted_email = email_regex.findall(url_text)
+    all_emails = []
+    for email in extracted_email:
+        all_emails.append(email[0])
+    length = len(all_emails)
+    print("\t [*] Number of Emails: %s\n" % length)
+    seen = set()
+    for email in all_emails:
+        if email not in seen:
+            seen.add(email)
+            email_file.write(email + '\n')
+
+
+# HTML PAGE READER
+def html_page_reader(url, i, email_regex):
+    try:
+        start_time = time.time()
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        requests = urllib.request.Request(url, None, headers)
+        responce = urllib.request.urlopen(requests)
+        url_html_page_reader = responce.read()
+        url_text = url_html_page_reader.decode()
+        print("%s.%s\t Fetched in:%s" % (i, url, (time.time() - start_time)))
+        extract_emails_from_url_text(url_text, email_regex)
+    except:
+        pass
+
+
+class WebScrapperLib:
+    # Emails Leech Function
     @staticmethod
-    def Requests_of_html():
-        user_url = str(input('[*] Enter Target URL to Scan:'))
-        urls = deque([user_url])
-        screapped_url = set()
-        emails = set()
-        count = 0
+    def emails_leech_function(url, i, email_regex):
         try:
-            while len(urls):
-                count += 1
-                if count == 100:
-                    break
-                url = urls.popleft()
-                screapped_url.add(url)
-                parts = urllib.parse.urlsplit(url)
-                base_url = '''{0.scheme}'://'{0.netloc}''', format(parts)
-
-                path = url[:url.rfind('/') + 1] if '/' in parts.path else url
-                print('[%d] Processing %s' % (count, url))
-
+            html_page_reader(url, i, email_regex)
+        except urllib.error.HTTPerror as err:
+            if err.code == 404:
                 try:
-                    response = requests.get(url)
-                except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-                    continue
-                new_emails = set(re.findall(r'[a-z0-9\. \-+_]+@[a-z0-9\. \-+_]+\.[a-z]', response.text, re.I))
-                emails.update(new_emails)
-
-                soup = BeautifulSoup(response.text, features='lxml')
-
-                for anchor in soup.find_all('a'):
-                    link = anchor.attrs['href'] if 'herf' in anchor.attrs else ''
-                if link.startswith('/'):
-                    link = path + link
-                    if not link in url and not link in screapped_url:
-                        urls.append(link)
-        except KeyboardInterrupt:
-            print("[-] Closing!")
-
-        for mail in emails:
-            print(mail)
+                    url = 'http://webcache.googleusercontent.com/search?q=cache:' + url
+                    html_page_reader(url, i, email_regex)
+                except:
+                    pass
+            else:
+                pass
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import sys
-from WebScrapperLib import WebScrapper
+import re
+import WebScrapperLib
+import time
+
+email_regex = re.compile(r'''
+    #example :
+    #something-.+_@somedomin.com
+    (
+    ([a-zA-Z0-9_.+]+
+    @
+    [a-zA-Z0-9_.+]+)
+    )
+    ''', re.VERBOSE)
+
 if __name__ == '__main__':
-    WebScrapper.Requests_of_html()
+    start = time.time()
+    url_file = open("urls.txt", 'r')
+    i = 0
+    for url_link in url_file.readlines():
+        url_link = url_link.strip('\'"')
+        i = i + 1
+        WebScrapperLib.WebScrapperLib.emails_leech_function(url_link, i, email_regex)
+    print("Elapsed Time: %s" % (time.time() - start))
+
+    url_file.close()
+    WebScrapperLib.email_file.close()
+
