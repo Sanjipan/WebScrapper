b87821725540fd45254dd437eececf69dc75ba99
Sanjipan <sanjipan2003@gmail.com>
2022-11-19 10:27:23 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import sys
 from WebScrapperLib import WebScrapper
 if __name__ == '__main__':
-
-    # for test
-    WebScrapper.parser('https://quotes.toscrape.com', headers={'user-agent': 'tiny scraper'})
+    pass
