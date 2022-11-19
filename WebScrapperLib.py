e39377e5f5f818388938a79b7c990f7db8fa4d9f
Sanjipan <sanjipan2003@gmail.com>
2022-11-19 10:26:31 +0530

    initial commit

--- WebScrapperLib.py
+++ WebScrapperLib.py
@@ -1,28 +1,6 @@
-from urllib.request import Request
-from urllib.request import urlopen
-
-
+from bs4 import BeautifulSoup
 class WebScrapper:
-    # Parser
     @staticmethod
-    def parser(url, *args, **kwargs):
-        try:
-            # Create Request Object
-            request = Request(url, **kwargs)
-
-            # Print Debug info
-            print('[HTTP "%s" to URL: %s] ' % (Request.__dict__['get_method'](Request), url))
-
-            # Make HTTP request to the target URL
-            response = urlopen(request).read().decode(encoding='utf-8', errors='ignore')
-
-            # init regex to prase HTML
-            regex = r'''(< *\w+( +\w+( *= *[\"|'][^\"|^']+[\"|'])?)* */? *>)([^<]*)'''
-            try:
-                regex = args[0]
-            except:
-                pass
-        except Exception as e:
-            print('[Error]:'+str(e))
+    def
 
 
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from WebScrapperLib import WebScrapper
 if __name__ == '__main__':
 
     # for test
-    WebScrapper.parser('https://enckllaec924.x.pipedream.net/', headers={'user-agent': 'tiny scraper'})
+    WebScrapper.parser('https://quotes.toscrape.com', headers={'user-agent': 'tiny scraper'})
