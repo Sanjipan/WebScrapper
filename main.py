58d60900c89971bdf79f1c4ba13ef81d7a24f7b7
Sanjipan <sanjipan2003@gmail.com>
2022-11-19 09:18:03 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
 from WebScrapperLib import WebScrapper
 if __name__ == '__main__':
-    pass
+
+    # for test
+    WebScrapper.parser('https://enckllaec924.x.pipedream.net/', headers={'user-agent': 'tiny scraper'})
